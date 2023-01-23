8a84da227f50085397950713c4b095e3a9c55cfb
tinamorlock <tinamorlock@gmail.com>
2023-01-23 14:24:07 -0600

    Tech Academy Daily Report Console App

---
+++ TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TechAcademyDailyReport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // tells user what this program is about
+
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            // Asks for student's name
+
+            Console.WriteLine("\n\nWhat is your name?");
+            string studentName = Console.ReadLine();
+
+            // Asks for student's course name
+
+            Console.WriteLine("What course are you on?");
+            string studentCourse = Console.ReadLine();
+
+            // asks what page student is on
+
+            Console.WriteLine("What page number are you on?");
+            string studentPageNum = Console.ReadLine();
+
+            // asks if student needs help
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            string studentHelp = Console.ReadLine();
+
+            // asks about positive experiences
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string studentPositive = Console.ReadLine();
+
+            // space for student to give additional feedback
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string studentOtherFeedback = Console.ReadLine();
+
+            // asks how many hours student studied
+
+            Console.WriteLine("How many hours did you study today?");
+            string studentStudied = Console.ReadLine();
+
+            // thank you message shown on screen
+
+            Console.WriteLine("\n\nThank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
