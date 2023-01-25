38a2fff652262aa633bdba8f9a0fea95a9e1b487
tinamorlock <tinamorlock@gmail.com>
2023-01-25 14:57:31 -0600

    Script that uses WHILE and DO WHILE loops.

---
+++ WhileStatements/WhileStatements/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WhileStatements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // run a while loop that displays a happy birthday message for the first 10 years
+
+            // initializing for first birthday
+
+            int childAge = 1;
+            bool checkAge = childAge <= 10;
+
+            // tested and runs for the first 10 birthdays
+
+            Console.WriteLine("These birthday messages use a WHILE loop.\n\n");
+
+            while (checkAge)
+            {
+                Console.WriteLine("Happy Birthday! You are " + childAge.ToString() + " years old!");
+                childAge++;
+                checkAge = childAge <= 10;
+            }
+            Console.ReadLine();
+
+            // same task with a do while loop
+
+            int childAge2 = 1;
+            bool checkAge2 = childAge2 <= 10;
+
+            // tested and runs for the first 10 birthdays
+
+            Console.WriteLine("These birthday messages use a DO WHILE loop.\n\n");
+
+            do
+            {
+                Console.WriteLine("Happy Birthday! You are " + childAge2.ToString() + " years old!");
+                childAge2++;
+                checkAge2 = childAge2 <= 10;
+            }
+            while (checkAge2);
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
