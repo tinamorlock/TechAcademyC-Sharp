dbbca35070c7edc102469c64792eb8a52069232c
tinamorlock <tinamorlock@gmail.com>
2023-02-07 00:04:43 -0600

    Working with C# methods

---
+++ MainMethod/MainMethod/MathOps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class MathOps
+    {
+        // takes in integer, does a math op, and returns integer
+
+        public int Adding(int userAdd)
+        {
+            return userAdd + 5000;
+        }
+
+        // takes in decimal, does a math op, and returns integer
+
+        public int Totals(decimal userCalc)
+        {
+            return Convert.ToInt32(1000.98M - userCalc);
+        }
+
+        // takes in string, converts to int, do math op, then return integer
+
+        public int stringMath(string integer)
+        {
+            int stringNumber = Convert.ToInt32(integer);
+            return stringNumber * stringNumber * 2;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOps addNumbers = new MathOps();
+            MathOps totalNumbers = new MathOps();
+            MathOps stringNum = new MathOps();
+
+            // passing integer to Adding(), then displays result on the screen
+
+            Console.WriteLine("Result from Adding(): " + addNumbers.Adding(1000));
+
+            // passing decimal to Totals(), then displays result on the screen
+
+            Console.WriteLine("Result from Totals(): " + totalNumbers.Totals(500.55M));
+
+            // passing int as a string to stringMath(), then displays integer that gets returned from the method
+
+            Console.WriteLine("Result from stringMath(): " + stringNum.stringMath("90"));
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
