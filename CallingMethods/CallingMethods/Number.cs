62acc99b160524f7ddbc2460a06b28781cb5dd9a
tinamorlock <tinamorlock@gmail.com>
2023-02-06 18:39:22 -0600

    Setting up class with three math methods to do some calculations

---
+++ CallingMethods/CallingMethods/Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    public class Number
+    {
+        // setting up three methods to take input from the user so we can do some calculations
+        // these are the methods we will be calling from the main program
+
+        public int Addition(int inputNumber)
+        {
+            return inputNumber + 59;
+        }
+        public int Subtract(int inputNumber)
+        {
+            return 100 - inputNumber;
+        }
+        public int Multiply(int inputNumber)
+        {
+            return inputNumber * 29;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // setting up instance of the Number class so we can call it after we get the user input
+
+            Number doMath = new Number();
+
+            // getting user to input number so we can call the methods
+
+            Console.WriteLine("Enter a number so we can do some math!");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition method result: " + doMath.Addition(userNumber));
+            Console.WriteLine("Subtract method result: " + doMath.Subtract(userNumber));
+            Console.WriteLine("Multiply method result: " + doMath.Multiply(userNumber));
+            Console.ReadLine();
+        }
+    }
+}
