9d79e60ce4a1c8a1ed0f227a0d364b3b9e34471b
tinamorlock <tinamorlock@gmail.com>
2023-02-07 18:32:47 -0600

    sending parameters to void methods

---
+++ VoidMethods/VoidMethods/Mathematics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethods
+{
+    internal class Mathematics
+    {
+        // method takes two integers as parameters
+        // does math op on first integer
+        // displays second integer on the screen
+
+        public void AddingTwo(int numOne, int numTwo)
+        {
+            int numThree = numOne * 5;
+            Console.WriteLine("The second number sent to the AddingTwo method: " + numTwo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethods/VoidMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathematics mathOne = new Mathematics();
+            // calls method, specifying numbers
+
+            mathOne.AddingTwo(9, 20);
+
+            // calls method, specifying parameters by name
+            int oneNum = 90;
+            int twoNum = 98;
+            mathOne.AddingTwo(oneNum, twoNum);
+        }
+    }
+}
