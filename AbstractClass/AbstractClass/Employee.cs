de665f2f0d971ee6aa00294b89ccc3c5fb546d19
tinamorlock <tinamorlock@gmail.com>
2023-02-09 14:57:55 -0600

    setting up abstract classes and methods

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // inherits Person class
+
+    public class Employee : Person
+    {
+        // overrides abstract method from Person
+
+        public override void SayName()
+        {
+            // prints name to the console
+
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // adding an instance of the Employee class
+
+            Employee newEmp = new Employee();
+
+            // setting up new object newEmp
+
+            newEmp.firstName = "Sample";
+            newEmp.lastName = "Student";
+
+            // prints newEmp data to console
+
+            newEmp.SayName();
+        }
+    }
+}
