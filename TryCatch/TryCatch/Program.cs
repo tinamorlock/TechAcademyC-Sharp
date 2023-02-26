1cc25fda0d93681985bf8822e8e308da1bf2a591
tinamorlock <tinamorlock@gmail.com>
2023-02-26 13:41:06 -0600

    creating Exception classes to customize error messages

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompting user for their age
+
+            try
+            {
+                Console.WriteLine("What is your age? ");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge == 0) { throw new ZeroException(); }
+                else if (userAge < 0) { throw new NegativeException(); }
+                else { Console.WriteLine("You entered a correct age!"); }
+                Console.ReadLine();
+            }
+            catch (ZeroException)
+            {
+                Console.WriteLine("You can't be zero years old!");
+                Console.ReadLine();
+                return;
+            }
+            catch (NegativeException)
+            {
+                Console.WriteLine("You can't be negative years old!");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Did you enter the right age?");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
